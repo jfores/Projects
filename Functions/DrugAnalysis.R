fccfca25bd976fdd6ade500e657778c1b24d16dd
jfores <fores.martos.jaume@gmail.com>
2021-03-12 18:39:13 +0100

    added multple changes in multiple scripts

--- Functions/DrugAnalysis.R
+++ Functions/DrugAnalysis.R
@@ -8,3 +8,15 @@ get_mapping_LINCS_DB <- function(x_All_LINCS){
   return(x_All_LINCS)
 }
 
+#Given an ICD code or a set of ICD codes porvided as a strig separated by a bar | (example "331.0|692.71") it returns the Drugbank codes for its drug indications reported in MEDI-AN Hig precission set.
+
+from_ICD_to_Indicated_DB <- function(icd_code,x_main_an_hp_drugs_all){
+  x_main_an_hp_drugs_all <- x_main_an_hp_drugs_all[grepl(icd_code,x_main_an_hp_drugs_all$ICD9),]
+  out_list <- list(unique(x_main_an_hp_drugs_all$primary_key))
+  names(out_list) <- paste(unique(x_main_an_hp_drugs_all$ICD9),collapse = "-")
+  return(out_list)
+}
+
+#Ginen a set of drugbank IDs this function returns a list of the available LINCS L1000 signatures generated using this drug as a perturbation.
+
+
